ec0c77faaeb86c5d49b089b68a803838d8e77394
Arsalaann <arsalankhanxk@gmail.com>
2023-06-18 16:16:05 +0530

    Arrays

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Welcome");
+//generate Random number then, find max and min
+let arr=new Array(10);
+for(var i=0;i<10;i++)
+    arr[i]=Math.floor((Math.random()*1001)+99);
+arr.sort();
+console.log(arr[0],arr[1]);
+console.log(arr[8],arr[9]);
